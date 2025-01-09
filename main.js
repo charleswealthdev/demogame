6a37c3b3a7f2e286ea529832a48454b52b4afcea
charleswealthdev <charleswealthinternational@gmail.com>
2025-01-09 11:59:23 +0100

    added back

--- main.js
+++ main.js
@@ -4,23 +4,21 @@ import { initializeLandingPageScene } from './src/landingpage';
 
 // Select the hero container, landing page, and game page containers
 const landingPage = document.getElementById('landingPage');
-const gamePage = document.getElementById('hero');
-
-createHeroScene(gamePage);
+const gamePage = document.getElementById('gamePage');
 
 // Initialize the landing page Three.js scene
-// initializeLandingPageScene(landingPage);
+initializeLandingPageScene(landingPage);
 
-// // Handle the start button click to transition to the game page
-// const startButton = document.getElementById('startButton');
-// if (startButton) {
-//   startButton.addEventListener('click', () => {
-//     // Hide the landing page and show the game page
-//     landingPage.remove();
-//     landingPage.style.display = 'none';
-//     gamePage.style.display = 'block';
+// Handle the start button click to transition to the game page
+const startButton = document.getElementById('startButton');
+if (startButton) {
+  startButton.addEventListener('click', () => {
+    // Hide the landing page and show the game page
+    landingPage.remove();
+    landingPage.style.display = 'none';
+    gamePage.style.display = 'block';
 
-//     // Initialize the hero game scene inside the game page
-//     createHeroScene(gamePage);
-//   });
-// }
+    // Initialize the hero game scene inside the game page
+    createHeroScene(gamePage);
+  });
+}
